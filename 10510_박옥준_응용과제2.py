7acbbc069e2bf02f0de60964c7504a8095f0dbf0
10510man <107024862+10510man@users.noreply.github.com>
2022-08-24 11:23:58 +0900

    Delete 10510_박옥준_응용과제2.py

--- 10510_박옥준_응용과제2.py
+++
@@ -1,6 +0,0 @@
-arr = [['일', 70, 80, None], ['이', 95, 85, None],['삼', 75, 60, None]]
-for i in range(3):
-    arr[i][3] = (arr[i][1] + arr[i][2])/2
-    for j in range(4):
-        print(arr[i][j], end = '\t')
-    print('\n')
